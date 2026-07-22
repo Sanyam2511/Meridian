626e4cbe2dc46f287f37cdcbb817bde0d3ad9de2
Sanyam2511 <sanyamphartyal822@gmail.com>
2026-07-22 15:36:06 +0530

    Initial commit for Meridian architecture

---
+++ optimization-engine/src/main/java/com/meridian/optimization/OptimizationEngineApplication.java
@@ -0,0 +1,13 @@
+package com.meridian.optimization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizationEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimizationEngineApplication.class, args);
+	}
+
+}
